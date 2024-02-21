a883ce5d11fefcdc8812a857ad195910e896eb29
ElaineLeandro <elainerleandro1@gmail.com>
2024-02-21 16:28:09 -0300

    feat: fluxo de controle

---
+++ cordehouse/javaScript/controleFluxo/script/index.js
@@ -0,0 +1,12 @@
+const umNumero = 5
+
+// comparando se umNumero é igual a 5
+if(umNumero == 5){
+    console.log("verá esta mensagem");
+};
+
+//comparando se unNumero é igual a 6
+
+if( umNumero == 6){
+    console.log("não verá esta mensagem..")
+}
---
+++ cordehouse/javaScript/funcoes/script/atividade.js
@@ -0,0 +1,20 @@
+/**
+ * Criar um algoritmo com uma condicional
+ * 
+ * Desenvolva um sistema que:
+ * 
+ * Solicite um número através do prompt
+ * Verificar se está entre 10 e 50
+ * Em caso positivo, mostre uma mensagem indicativa em um alert
+ * Em caso positivo, mostre uma mensagem indicativa en um console
+ */
+
+let numero = prompt("Por favor, insira um número:")
+
+if(numero >= 10 && numero <= 50){
+    alert("O número está entre 10 e 50!")
+
+    console.log("O número está entre 10 e 50!")
+}else{
+    console.log("O número não está entre 10 e 50.");
+}
